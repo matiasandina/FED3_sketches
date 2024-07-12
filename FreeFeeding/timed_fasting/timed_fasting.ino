b5230856197abd7a7117a6efc7c2dfd215aebe96
Matias Andina <matiasandina@gmail.com>
2024-07-12 11:29:08 -0400

    forgot fw stuff in the timed_fasting.ino

--- FreeFeeding/timed_fasting/timed_fasting.ino
+++ FreeFeeding/timed_fasting/timed_fasting.ino
@@ -12,7 +12,7 @@
 */
 
 #include "FED3.h"                                       //Include the FED3 library 
-String sketch = "FreeFeed";                             //Unique identifier text for each sketch (this will show up on the screen and in log file)
+String sketch = "TimedFast";                             //Unique identifier text for each sketch (this will show up on the screen and in log file)
 String fasting_start = "fasting_start";
 String fasting_stop = "fasting_stop";
 // Declare global variables
@@ -27,20 +27,25 @@ void setup() {
   fed3.DisplayPokes = false; // Customize the DisplayPokes option to 'false'
   fed3.timeout = 3; // Set a timeout period (in seconds) after each pellet is taken
 
+  // Turn to true if you are using FEDWatcher
+  fed3.setSerial(true);
+
   // Set fasting_start_dt and fasting_stop_dt using the methods from the FED3 object
   fasting_start_dt = fed3.SetEventDt(fasting_start);
   fasting_stop_dt = fed3.SetEventDt(fasting_stop);
 
-  // Correctly format and print the datetime info to Serial
-  char time_buffer[100];
-  Serial.println("Fasting Start");
-  sprintf(time_buffer, "%d-%02d-%02d %02d:%02d", fasting_start_dt.year(), fasting_start_dt.month(), fasting_start_dt.day(), fasting_start_dt.hour(), fasting_start_dt.minute());
-  Serial.println(time_buffer);
-  Serial.println("Fasting Stop");
-  sprintf(time_buffer, "%d-%02d-%02d %02d:%02d", fasting_stop_dt.year(), fasting_stop_dt.month(), fasting_stop_dt.day(), fasting_stop_dt.hour(), fasting_stop_dt.minute());
-  Serial.println(time_buffer);
   // Display the timed fasting session
   //fed3.DisplayTimed = true;
+
+
+  // Correctly format and print the datetime info to Serial
+  char time_buffer[100];
+  // Serial.println("Fasting Start");
+  // sprintf(time_buffer, "%d-%02d-%02d %02d:%02d", fasting_start_dt.year(), fasting_start_dt.month(), fasting_start_dt.day(), fasting_start_dt.hour(), fasting_start_dt.minute());
+  // Serial.println(time_buffer);
+  // Serial.println("Fasting Stop");
+  // sprintf(time_buffer, "%d-%02d-%02d %02d:%02d", fasting_stop_dt.year(), fasting_stop_dt.month(), fasting_stop_dt.day(), fasting_stop_dt.hour(), fasting_stop_dt.minute());
+  //Serial.println(time_buffer);
 }
 
 
@@ -50,12 +55,12 @@ void loop() {
   bool isFasting = now >= fasting_start_dt && now <= fasting_stop_dt;
   fed3.DisplayFeedingStatus(isFasting); // This might get plotted over
   if (isFasting) {
-      Serial.println("Fasting and Checking Well");
+      //Serial.println("Fasting and Checking Well");
       fed3.CheckWell();
       // this will only show when not being cleared (e.g., while )
       fed3.DisplayFeedingDT(fasting_start_dt, fasting_stop_dt); 
   } else {
-      Serial.println("Feeding is Active");
+      //Serial.println("Feeding is Active");
       fed3.Feed(); // Drop pellet 
       fed3.DisplayFeedingDT(fasting_start_dt, fasting_stop_dt); 
   }
